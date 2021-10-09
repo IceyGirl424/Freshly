34fdf1ea7addcf99ee3398a54b280b9b9108bc4b
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2021-10-09 15:30:52 -0700

    Restored backend Files

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -6,5 +6,5 @@ class User(models.Model):
     email = models.EmailField(max_length=50)
     password = models.CharField(max_length=50)
     birthday = models.DateField()
-    phone_number = models.IntegerField(max_length=15)
+    phone_number = models.IntegerField()
     username = models.CharField(max_length=50)
