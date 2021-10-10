5ae7fcc5b52c38ae4c80e9c09d7bf750b3b3080d
Emily Cai <iac.ylime@gmail.com>
2021-10-10 02:05:58 -0700

    made login logo smaller and added search bar and title to Search page

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,17 @@
+import './Search.css';
+
 function Search() {
     return (
-        <div>
-            
+        <div id="SearchPage" style={{backgroundImage:"url(https://cdn.discordapp.com/attachments/893663423164989440/896574382498000916/image0.png)"}}>
+            <div>
+                <h3 className="SearchPageHeader">Freshly</h3>
+            </div>
+            <div>
+                <label>Search:</label>
+            </div>
+            <form>
+                <input type="text" name="SearchBar" className="SearchBarCSS"/>
+            </form>
         </div>
     );
 }
